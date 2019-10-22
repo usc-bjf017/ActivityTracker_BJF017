0fef47b155ae58662cd40c1bd8b1e46436d2c81e
Benjamin <mail.benjaminfinlay@gmail.com>
2019-10-22 23:04:19 +1000

    Activity Class and UI Layouts

---
+++ app/src/main/java/com/benjaminfinlay/activitytracker/FragmentActivity.java
@@ -0,0 +1,113 @@
+package com.benjaminfinlay.activitytracker;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FragmentActivity.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FragmentActivity#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentActivity extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String TITLE = "title";
+    private static final String PLACE = "place";
+    private static final String DATE = "date";
+
+    // TODO: Rename and change types of parameters
+    private String title;
+    private String place;
+    private String date;
+
+    private OnFragmentInteractionListener mListener;
+
+    public FragmentActivity() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param title Title of the activity.
+     * @param place Place/Location of the activity.
+     * @param date Date of the activity.
+     * @return A new instance of fragment FragmentActivity.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentActivity newInstance(String title, String place, String date) {
+        FragmentActivity fragment = new FragmentActivity();
+        Bundle args = new Bundle();
+        args.putString(TITLE, title);
+        args.putString(PLACE, place);
+        args.putString(DATE, date);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            title = getArguments().getString(TITLE);
+            place = getArguments().getString(PLACE);
+            date = getArguments().getString(DATE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_activity, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/benjaminfinlay/activitytracker/MainActivity.java
+++ app/src/main/java/com/benjaminfinlay/activitytracker/MainActivity.java
@@ -1,5 +1,14 @@
 package com.benjaminfinlay.activitytracker;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +17,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        GetLocation();
         setContentView(R.layout.fragment_activity);
+    }
+
+    private Location GetLocation() {
+        // Get LocationManager object
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 10);
+                return null;
+            }
+        }
+
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+        if (myLocation != null) {
+            return myLocation;
+        } else {
+            return null;
+        }
+
+        //myLatitude = myLocation.getLatitude();
+        //myLongitude = myLocation.getLongitude();
     }
 }
